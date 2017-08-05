da0471e4a9f45d294490fa42abf5f54ee1b2fccf
mclinch <michael.clinch@gmail.com>
2017-08-04 21:56:51 -0700

    files sequalized and database test

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,36 @@
-var orm = require("../config/orm");
+// // var orm = require("../config/orm");
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
+// var burger = {
+//   all: function(cb) {
+//     orm.all("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   create: function(vals, cb) {
+//     orm.create("burgers", "burger_name", vals, function(res) {
+//       cb(res);
+//     });
+//   },
+//   update: function(objColVals, condition, cb) {
+//     orm.update("burgers", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
+// module.exports = burger;
+module.exports = function (sequelize, DataTypes) {
+    let burgers = sequelize.define('burger', {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[1]
+            }
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
     });
-  },
-  create: function(vals, cb) {
-    orm.create("burgers", "burger_name", vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
-};
-module.exports = burger;
+    return burgers;
+}
